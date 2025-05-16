ae682537e458bd6027a467b53513cd069f6efea4
Hemanth Kumar <mr.hk339@gmail.com>
2025-05-16 17:58:40 +0530

    Authentication Routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import {createServer} from 'node:http';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import {Server} from 'socket.io';
 import {connectToSocket} from './controllers/socketManger.js';
+import userRoutes from './routes/users.route.js';
 
 const app = express();
 const server = createServer(app);
@@ -14,16 +17,22 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-app.get('/home', (req, res) => {
-    return res.send('Hello World'); 
-});
+app.use('/api/v1/users', userRoutes);
 
 const start = async () => {
-    const connectionDb = 'process.env.MONGODB_URI';
-    console.log("MongoDB connected");
-    app.listen(app.get("port"), () => {
-        console.log('Server is running on port 3000');
-    });
-}
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
 
+        console.log("✅ MongoDB connected");
+
+        const PORT = app.get("port") || 3000;
+        app.listen(PORT, () => {
+            console.log(`🚀 Server is running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("MongoDB connection failed:", error.message);
+        process.exit(1); // Exit the process if DB connection fails
+    }
+};
 start();
